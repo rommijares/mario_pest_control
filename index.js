a10aa7c475e02cd920f622317c8cbff9cf839a44
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-10-02 19:39:39 -0400

    commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 document.getElementById('myForm').addEventListener('submit', function(event) {
     event.preventDefault()
+})
+document.getElementById('button').addEventListener('click', function() {
     let goombaT = document.getElementById('goombaTotal').value
     let price = goombaT * 5; 
     let bobombT = document.getElementById('bobTotal').value
@@ -8,4 +10,12 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
     let price3 = cheepsT * 11;   
     var totalPrice = price + price2 + price3 
     document.getElementById('total').textContent = totalPrice
+});
+
+
+document.getElementById('clearButton').addEventListener('click', function() {
+    document.getElementById('goombaTotal').value = " "
+    document.getElementById('bobTotal').value = " "
+    document.getElementById('cheepTotal').value = " " 
+    document.getElementById('total').textContent = " "
 });
